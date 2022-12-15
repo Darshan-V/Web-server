c910ba3488c54003ce444f1b1ca3ebd4e6b64d9e
Darshan-V <mr.darshanv@gmail.com>
2022-12-15 17:52:01 +0530

    added more routes/paths options

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
 import myExpress from './lib/myexpress.js'
 const app = myExpress()
 
-app.get('/', function (req, res) {
-  res.send('Good morning')
-})
-
-app.get('/morning', function (req, res) {
-  console.log(req.params)
-  res.send('Good morning ' + req.params['name'])
-})
-
-app.get('/greet', function (req, res) {
-  console.log(req.params)
-  res.send(req.params['greeting'] + ' ' + req.params['name'])
+app.get('/morning/:name', function (req, res) {
+  res.writeHead(200)
+  res.end('Good morning, ' + req.params['name'])
 })
 app.listen(3000)

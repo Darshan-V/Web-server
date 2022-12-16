4e23d705643ff50563a834cf13c3f71acfaa13ef
Darshan-V <mr.darshanv@gmail.com>
2022-12-16 14:10:44 +0530

    added more methods

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
 import myExpress from './lib/myexpress.js'
+// import { fileURLToPath } from 'url'
+// import { dirname } from 'path'
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
+
 const app = myExpress()
 
 app.get('/morning/:name', function (req, res) {
-  res.writeHead(200)
-  res.end('Good morning, ' + req.params['name'])
+  res.send('Good morning, ' + req.params['name'])
+})
+
+app.put('/', (req, res) => {
+  let a = 'some string'
+  res.json(a)
+})
+
+app.delete('/del', (req, res) => {
+  let b = 'delete something'
+  res.json(b)
 })
 app.listen(3000)
